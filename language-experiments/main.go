aede0327b6e2cff767ed56d7566367306936b559
cdugga <duggan.colin@gmail.com>
2020-02-28 14:42:52 +0000

    add language experiments

---
+++ language-experiments/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type contactAddr struct {
+	email string
+}
+
+type dev struct {
+	name string
+	email contactAddr
+}
+
+func main() {
+	//httpRequest("http://www.google.com")
+
+	testPointerUpdate()
+}
+
+func testPointerUpdate() {
+	d := createDev()
+	// will update reference and subsequent requests will use the original memory location
+	fmt.Println("**************** UPDATING ONLY REFERENCE TO MEMORY (remain lowercase)*************************")
+	fmt.Println(d.toUpper())
+	fmt.Println("Original...",d.email.email)
+	fmt.Println("Original...",d.name)
+
+	// will update memory location and subsequent requests will use the new updates
+	fmt.Println("**************** UPDATING LOCATION IN MEMORY (now all UPPERCASE) *************************")
+	d.toUpperWithPointer()
+	fmt.Println("Updated...", d.email.email)
+	fmt.Println("Updated...", d.name)
+}
+
+
+
+func createDev() dev {
+	d := dev{
+		name: "Colin",
+		email: contactAddr{
+			email: "duggan.colin@gmail.com",
+		},
+	}
+	return d
+}
+
+func httpRequest(link string){
+	resp, err := http.Get(link)
+	if err != nil {
+		fmt.Println("Error", err)
+		os.Exit(1)
+
+	}
+	fmt.Println(link, resp.Status)
+	io.Copy(os.Stdout, resp.Body)
+
+}
+
+// will update reference to pointer
+func (d dev) toUpper() (string, string) {
+	return strings.ToUpper(d.name), strings.ToUpper(d.email.email)
+}
+
+// will update the point in memory
+func (d *dev) toUpperWithPointer() {
+	(*d).email.email = strings.ToUpper(d.email.email)
+	(*d).name = strings.ToUpper(d.name)
+}
