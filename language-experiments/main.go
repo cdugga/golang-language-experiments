cc263a75ae97688b8e9048d1c377a6d1f3f03775
cdugga <duggan.colin@gmail.com>
2020-02-28 17:17:55 +0000

    if with assignment and conditional

--- language-experiments/main.go
+++ language-experiments/main.go
@@ -20,32 +20,48 @@ type dev struct {
 func main() {
 	//httpRequest("http://www.google.com")
 	//testPointerUpdate()
-	testForLoop()
+	//testForLoop()
+	ifExpr()
 }
 
-func testForLoop() {
+func ifExpr() string{
+	// execute short statement before condition
+	if v := "colin"; v == strings.ToLower("COLIN") {
+		fmt.Println(v)
+		return v
+	}
+	return "no match"
+}
 
+
+func testForLoop() {
 	colors := map[string]string{
 		"red" : "#fffbbb",
 		"green": "#f12345",
 		"white": "#ffffff",
 	}
-
 	// using both object values
 	for color, hex := range colors {
 		fmt.Println("Hex for ", color, "is ", hex)
 	}
-
 	// ignoring hex value
 	for color, _ := range colors {
 		fmt.Println("Color:", color)
 	}
-
 	// explicitly defining loop boundary
 	for i := 0; i <10; i++ {
 		fmt.Println("Iterating...", i)
 	}
-
+	// using implicit init and post statements
+	sum :=1
+	for ;  sum < 5; {
+		sum += sum
+	}
+	fmt.Println(sum)
+	// above can be replaced with
+	for sum < 5 {
+		sum +=sum
+	}
 }
 
 
