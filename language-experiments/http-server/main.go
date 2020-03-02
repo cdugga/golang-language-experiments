a650ddcb7d2fc625d792e17822b236cb44aa3f14
cdugga <duggan.colin@gmail.com>
2020-03-02 20:46:14 +0000

    return json encoded string

--- language-experiments/http-server/main.go
+++ language-experiments/http-server/main.go
@@ -1,6 +1,7 @@
 package http_server
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
@@ -8,6 +9,11 @@ import (
 )
 
 type GreetingHandler string
+
+const devs = `[
+	{"Title":"cdugga", "Skill": "full stack developer"},
+	{"Title": "gholly", "Skill": "full stack teacher"}
+]`
 
 func (g GreetingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w, g)
@@ -17,23 +23,23 @@ func headerHandler(w http.ResponseWriter, r *http.Request){
 	for k,v := range r.Header {
 		fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
 	}
-
 	fmt.Fprintf(w, "Host = %q\n", r.Host)
 	fmt.Fprintf(w, "RemoteAddr= %q\n", r.RemoteAddr)
-
 	fmt.Fprintf(w, "\n\n\"Accept\" %q", r.Header["Accept"])
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(devs)
+
 }
 
 func RunServer(){
 	r := mux.NewRouter()
 
 	r.HandleFunc("/headers", headerHandler).Methods("GET")
+	r.HandleFunc("/devteam", handler).Methods("GET")
 
-	http.HandleFunc("/goworld", handler)
 	err := http.ListenAndServe("localhost:3002", r) //, GreetingHandler("Mayo 4 GO"))
 	if err != nil{
 		log.Fatal(err)
