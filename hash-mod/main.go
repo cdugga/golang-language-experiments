5b67ab50fc1239fec247e9e6e21d8681c13c7285
cdugga <duggan.colin@gmail.com>
2020-04-08 20:50:41 +0100

    create command and options

--- hash-mod/main.go
+++ hash-mod/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-
 	"fmt"
 	"github.com/urfave/cli/v2"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -21,13 +19,13 @@ func fetch(){
 	}
 	defer resp.Body.Close()
 
-	responseData, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	//responseData, err := ioutil.ReadAll(resp.Body)
+	//if err != nil {
+//		log.Fatal(err)//
+//	}
 
-	responseString := string(responseData)
-	fmt.Printf(responseString)
+	//responseString := string(responseData)
+	//fmt.Printf(responseString)
 
 }
 
@@ -36,24 +34,62 @@ func main() {
 
 	var name,language string
 	app := &cli.App{
+		Name: "Apigee Publish API CLI Utility",
+		Usage: "Create API Proxy Endpoints",
 		Commands: []*cli.Command{
 			{
 				Flags: []cli.Flag{
 					&cli.StringFlag{
 						Name:  "lang, l",
+						Aliases: []string{"l"},
 						Value: "english",
 						Usage: "Language for the greeting",
 						Destination: &language,
 					},
 					&cli.StringFlag{
-							Name:  "name, n",
+						Name:  "name, n",
+						Aliases: []string{"n"},
 						Usage: "Enter name",
 						Destination: &name,
 					},
 				},
-				Name:    "print",
+				Name:    "proxy",
 				Aliases: []string{"p"},
-				Usage:   "print statistics",
+				Usage:   "Create Apigee API Proxy",
+
+				Action: func(c *cli.Context) error {
+					//name := "Nefertiti"
+					fetch()
+					if c.NArg() > 0 {
+						name = c.Args().Get(0)
+					}
+					if language == "spanish" {
+						fmt.Println("Hola", name)
+					} else {
+						fmt.Println("Hello", name)
+					}
+					return nil
+				},
+			},
+			{
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "lang, l",
+						Aliases: []string{"l"},
+						Value: "english",
+						Usage: "Language for the greeting",
+						Destination: &language,
+					},
+					&cli.StringFlag{
+						Name:  "name, n",
+						Aliases: []string{"n"},
+						Usage: "Enter name",
+						Destination: &name,
+					},
+				},
+				Name:    "product",
+				Aliases: []string{"pr"},
+				Usage:   "Create Apigee Product",
 
 				Action: func(c *cli.Context) error {
 					//name := "Nefertiti"
@@ -71,6 +107,7 @@ func main() {
 			},
 
 		},
+
 
 	}
 
