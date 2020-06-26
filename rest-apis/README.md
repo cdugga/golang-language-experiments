5e46891f533d472bf60f147910feeb3e2694c4a8
Colin Duggan <duggan.colin@gmail.com>
2020-06-26 16:24:14 +0100

    encode recieved json

--- rest-apis/README.md
+++ rest-apis/README.md
@@ -1,3 +1,31 @@
 Invoke-RestMethod -Uri http://localhost:8080/ -B "Colin" -Method POST
 
 Invoke-WebRequest http://localhost:8080/ -B "Colin" -Method POST
+
+//Pass params with Get
+$Parameters = @{
+  id = 'John'
+  Children = 'Abe','Karen','Jo'
+}
+Invoke-RestMethod -Uri http://localhost:8080/ -B $Parameters -Method GET
+
+
+
+$JSON = @'
+{
+ "@type":"login",
+ "name":"xxx@gmail.com",
+ "password":"yyy"
+}
+'@
+
+$body = @"
+{
+    "id": 1,
+    "name": "tea",
+    "description": "a nice cup of tea"   
+}
+"@
+
+
+Invoke-WebRequest -Uri "http://localhost:8080/" -Method Post -Body $body -ContentType "application/json"
--- rest-apis/handlers/product.go
+++ rest-apis/handlers/product.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"encoding/json"
 	"log"
 	"main.go/data"
 	"net/http"
@@ -16,14 +15,41 @@ func NewProduct(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p* Products) ServeHTTP(rw http.ResponseWriter, h * http.Request){
+func (p* Products) ServeHTTP(rw http.ResponseWriter, r * http.Request){
+	if r.Method == http.MethodGet {
+		p.GetProducts(rw, r)
+		return
+	}
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
 
-		lp := data.GetProducts()
+	rw.WriteHeader(http.StatusMethodNotAllowed)
 
-		d, err := json.Marshal(lp)
+}
 
-		if err != nil {
-			http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
-		}
-		rw.Write(d)
+func (p* Products) GetProducts(rw http.ResponseWriter, r * http.Request){
+
+	p.l.Println("Handle GET Requests")
+
+	lp := data.GetProducts()
+
+	err :=  lp.ToJSON(rw)  //json.Marshal(lp)
+
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Requests")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json string", http.StatusBadRequest)
+	}
+	p.l.Printf("Prod: %#v", prod)
 }
