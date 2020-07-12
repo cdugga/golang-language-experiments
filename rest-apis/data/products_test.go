1af89ce7675a629b145eb188cbc2894b1d8b5bbf
Colin Duggan <duggan.colin@gmail.com>
2020-07-12 18:03:14 +0100

    simple tests

--- rest-apis/data/products_test.go
+++ rest-apis/data/products_test.go
@@ -1,8 +1,43 @@
 package data
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
-func TestChecksValidation(t *testing.T){
+//p := &Product{
+//ID:          0,
+//Name:        "Tea",
+//Description: "",
+//Price:       1.0,
+//SKU:         "abs-abc-def",
+//CreatedOn:   "",
+//UpdatedOn:   "",
+//DeletedOn:   "",
+//}
+
+
+func TestMissingNameAndSKUError(t *testing.T){
+	p := &Product{
+		Description: "",
+		Price:       1.0,
+	}
+	err := p.Validate()
+	assert.Len(t, err, 2)
+}
+
+
+func TestMissingNameError(t *testing.T){
+	p := &Product{
+		Description: "",
+		Price:       1.0,
+		SKU:         "abs-abc-def",
+	}
+	err := p.Validate()
+	assert.Len(t, err, 1)
+}
+
+func TestValidProduct(t *testing.T){
 	p := &Product{
 		ID:          0,
 		Name:        "Tea",
@@ -14,11 +49,8 @@ func TestChecksValidation(t *testing.T){
 		DeletedOn:   "",
 	}
 
-	err := p.Validate()
-	if err != nil {
-		t.Fatal(err)
-	}
+	err:= p.Validate()
+	assert.NoError(t,err)
 
 }
-
 
