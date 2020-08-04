e0bf653750b79f924cd29d054d37c0adebeb4a9d
Colin Duggan <duggan.colin@gmail.com>
2020-08-04 21:17:02 +0100

    max sub array completion

--- algo_training/maxSubArray/maxSubArray.go
+++ algo_training/maxSubArray/maxSubArray.go
@@ -1,11 +1,34 @@
 package maxSubArray
 
+import (
+	"fmt"
+	"math"
+)
 
 func MaxSubArray(){
-
+	fmt.Println("MaximumSubarraySum([]int{})): ",MaximumSubarraySum([]int{}))
+	fmt.Println("MaximumSubarraySum([]int{})): ",MaximumSubarraySum([]int{-1,-1,-3,-4,-1}))
+	fmt.Println("MaximumSubarraySum([]int{})): ",MaximumSubarraySum([]int{1,3,4}))
+	fmt.Println("MaximumSubarraySum([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})): ",MaximumSubarraySum([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
 }
 
-
 func MaximumSubarraySum(numbers []int) int {
+	if len(numbers) == 0 { return 0}
+	isNegative := func(number int) bool {return math.Signbit(float64(number))}
+	arr := filter(numbers, isNegative)
+	if len(arr) == len(numbers){ return 0 }
+	if len(arr) == 0 && len(numbers) > 0 { var sum int; for _, s := range numbers { sum +=s }; return sum; }
 
+	var maxEndingHere, maxSoFar int ;
+	for _, s := range numbers{
+		maxEndingHere+=s;
+		if maxEndingHere < 0 { maxEndingHere = 0 }
+		if maxEndingHere > maxSoFar { maxSoFar = maxEndingHere}
+	}
+	return maxSoFar
+}
+
+func filter(numbers []int, test func(int)bool)(ret []int){
+	for _, number := range numbers { if test(number) { ret = append(ret, number)}}
+	return
 }
